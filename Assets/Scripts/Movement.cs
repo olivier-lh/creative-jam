6a8d666142368160e9d823e36e46455340948ec2
Vincent Anctil <vincent.anctil@polymtl.com>
2022-09-17 09:28:12 -0400

    automatic jump when colliding with jumpTriggerBox2D

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,10 +6,11 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
     private BoxCollider2D boxCollider2D;
-    private float movementSpeed = 1.0f;
-    private float jumpForce = 10.0f;
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private float jumpForce = 10.0f;
 
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private BoxCollider2D jumpTriggerBox2D;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +22,9 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-        Debug.Log(IsGrounded());
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2D.velocity = Vector2.up * jumpForce;
-       
-        }
+        transform.Translate(Vector2.right * movementSpeed * Time.deltaTime); // Constant forward movement
+
+        OnTriggerEnter2D(boxCollider2D);
     }
 
     private bool IsGrounded()
@@ -37,4 +34,22 @@ public class Movement : MonoBehaviour
             boxCollider2D.bounds.extents.y + extraHeigthText, platformLayerMask);
         return rayCastHit.collider != null;
     }
+
+    void Jump()
+    {
+        rigidbody2D.velocity = Vector2.up * jumpForce;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsGrounded() && collision.CompareTag("JumpTrigger"))
+        {
+            Jump();
+        }
+        else
+        {
+            Debug.Log("not triggered");
+        }
+    }
 }
+
